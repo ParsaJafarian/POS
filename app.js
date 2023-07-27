9a14d437714749aa4b865652ce311a9ee567d314
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-07-27 12:10:20 -0400

    Updating

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'hbc'
-})
+});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send("home");
