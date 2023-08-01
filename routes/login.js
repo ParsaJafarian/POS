b71568d4940c8ed90d68dca27dc30fcc276bc48f
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-01 09:08:29 -0400

    Implementing Flash messages with basic error handling

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const flash = require('connect-flash');
+
+router.use(flash());
 
 router.get('/', (req, res) => {
-    res.render('login', { message: req.flash('failure') });
+    res.render('login', { messages: req.flash('error') });
 });
 
 router.post('/', passport.authenticate('local', {
     successRedirect: '/profile',
     failureRedirect: '/login',
-    failureFlash: true
+    failureFlash: 'Incorrect employee number or password',
+    successFlash: 'Welcome!'
 }));
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -3,21 +3,25 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const db = require('../config/db');
 const { v4: uuid } = require('uuid');
+const flash = require('connect-flash');
 
-router.set('view engine', 'ejs');
-router.use()
+router.use(flash());
 
 router.get('/', (req, res) => {
-    res.render('login', { message: req.flash('failure') });
+    res.render('register', { messages: req.flash('error') });
 });
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     const hash = bcrypt.hashSync(req.body.password, 12);
-    const q = "INSERT INTO employees (id, first_name, last_name, email, phone, password) VALUES ?";
-    const data = [[uuid(), req.body.first_name, req.body.last_name, req.body.email, req.body.phone, hash]];
-    await db.query(q, [data]);
-    req.flash('success', 'You have successfully registered!');
-    res.redirect('/profile');
+    const q = "INSERT INTO employees (id, number, first_name, last_name, email, phone, password) VALUES ?";
+    const data = [[uuid(), req.body.number, req.body.first_name, req.body.last_name, req.body.email, req.body.phone, hash]];
+    db.query(q, [data])
+        .then(res => {
+            res.redirect('/login');
+        }).catch(err => {
+            req.flash('error', err.message);
+            res.redirect('register');
+        });
 });
 
 module.exports = router;
