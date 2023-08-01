f16a5543ffbf5cd0bfb08f47caa3d6249629761a
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-01 13:02:18 -0400

    Add profile.js route

---
+++ routes/profile.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { isLoggedIn } = require('../middleware');
+
+router.get('/', isLoggedIn, (req, res) => {
+    res.send('Profile page');
+});
+
+module.exports = router;
