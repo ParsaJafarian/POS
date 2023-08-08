d1818d8fd0ef7d4a78fbf00a61f378ca18f84ea9
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-08 11:03:02 -0400

    Change directories

--- database/hbc.sql
+++ database/hbc.sql
@@ -2,10 +2,10 @@ DROP DATABASE IF EXISTS hbc;
 CREATE DATABASE IF NOT EXISTS hbc;
 USE hbc;
 
-SOURCE C:/Users/Admin/Desktop/LaBaie/database/tables/customers.sql;
-SOURCE C:/Users/Admin/Desktop/LaBaie/database/tables/employees.sql;
-SOURCE C:/Users/Admin/Desktop/LaBaie/database/tables/transactions.sql;
-SOURCE C:/Users/Admin/Desktop/LaBaie/database/tables/products.sql;
-SOURCE C:/Users/Admin/Desktop/LaBaie/database/tables/index.sql;
+SOURCE C:/Users/Admin/Desktop/HBC/database/tables/customers.sql;
+SOURCE C:/Users/Admin/Desktop/HBC/database/tables/employees.sql;
+SOURCE C:/Users/Admin/Desktop/HBC/database/tables/transactions.sql;
+SOURCE C:/Users/Admin/Desktop/HBC/database/tables/products.sql;
+SOURCE C:/Users/Admin/Desktop/HBC/database/tables/transaction_products.sql;
 
 
