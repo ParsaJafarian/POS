59c4133e3c9bd985e8ea9983d1a42a5214dd546e
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-08 11:01:49 -0400

    Reorganize into smaller functions

--- public/scripts/productsList.js
+++ public/scripts/productsList.js
@@ -5,25 +5,29 @@ const input = document.querySelector('#productInput');
 const ol = document.querySelector('#products');
 const flashMessages = document.querySelector('.flash-messages');
 
+const showProduct = (res) => {
+    const li = document.createElement('li');
+    li.textContent = res.data.number + ' - ' + res.data.type + ' - ' + res.data.price + '$';
+    ol.appendChild(li);
+};
+
+const displayError = (err) => {
+    if (err.response) {
+        const { message = 'Empty Input' } = err.response.data;
+        const flashMessage = document.createElement('div');
+        flashMessage.classList.add('flash-message');
+        flashMessage.textContent = message;
+        flashMessages.appendChild(flashMessage);
+    } else {
+        console.log('An error occurred:', err.message);
+    }
+};
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
     if (flashMessages.children.length > 0) flashMessages.removeChild(flashMessages.children[0]);
     axios.get('/products/' + input.value) // Use relative URL
-        .then((res) => {
-            const li = document.createElement('li');
-            li.textContent = res.data.number + ' - ' + res.data.type + ' - ' + res.data.price + '$';
-            ol.appendChild(li);
-        })
-        .catch((err) => {
-            if (err.response) {
-                const { message = 'Empty Input' } = err.response.data;
-                const flashMessage = document.createElement('div');
-                flashMessage.classList.add('flash-message');
-                flashMessage.textContent = message;
-                flashMessages.appendChild(flashMessage);
-            } else {
-                console.log('An error occurred:', err.message);
-            }
-        });
+        .then((res) => showProduct(res))
+        .catch((err) => displayError(err));
 });
 
