725b4f85d43c8bfa660cfc72e0199a3185397b22
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-08 09:25:08 -0400

    Try to add functional error handling

--- routes/products.js
+++ routes/products.js
@@ -2,20 +2,17 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
 
-router.get('/:number', catchAsync(async (req, res) => {
+router.use('/:number', catchAsync(async (req, res, next) => {
     const number = req.params.number;
+    if(!number) throw new ExpressError('Invalid product number', 400);
     const q = 'SELECT * FROM products WHERE number = ?';
     const result = await db.query(q, [number]);
     const product = result[0][0];
-    if (!product) throw new Error('Product not found', 404);
+    if (!product) throw new ExpressError('Product not found', 404);
     res.send(product);
+    next();
 }));
-
-router.use((err, req, res, next) => {
-    req.flash('error', err.message);
-    res.redirect('/transactions/new');
-})
-
 
 module.exports = router;
