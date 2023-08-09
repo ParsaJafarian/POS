a0f6554e295fbb3691e038c3a533193da343bac8
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-08 21:38:56 -0400

    make everything functional

--- public/scripts/productsList.js
+++ public/scripts/productsList.js
@@ -9,41 +9,6 @@ const flashMessages = document.querySelector('.flash-messages');
 
 const addedProductsTexts = new Set();
 
-const makeDeleteBtn = (li) => {
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'X';
-    deleteBtn.className = 'btn btn-danger btn-sm delete-btn px-1 py-0';
-    deleteBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        ol.removeChild(li);
-        //li.firstchild is the span element that contains the product text
-        addedProductsTexts.delete(li.firstChild.textContent);
-    });
-    return deleteBtn;
-};
-
-const makeSpan = (productText) => {
-    const span = document.createElement('span');
-    span.className = 'product-text';
-    span.textContent = productText;
-    return span;
-};
-
-const makeFlexContainer = (productText) => {
-    const flexContainer = document.createElement('div');
-    flexContainer.className = 'd-flex justify-content-between align-items-center w-100';
-    flexContainer.appendChild(makeSpan(productText));
-    flexContainer.appendChild(makeDeleteBtn());
-    return flexContainer;
-};
-
-const makeLi = (productText) => {
-    const li = document.createElement('li');
-    li.className = 'list-group-item d-flex justify-content-between align-items-center';
-    const flexContainer = makeFlexContainer(productText);
-    li.appendChild(flexContainer);
-    return li;
-};
 
 const addProduct = (res, is_return) => {
     const productText = is_return ?
@@ -52,8 +17,28 @@ const addProduct = (res, is_return) => {
 
     if (addedProductsTexts.has(productText)) throw new Error('Product already added');
 
-    const li = makeLi(productText, addedProductsTexts);
+    const li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center';
 
+    const flexContainer = document.createElement('div');
+    flexContainer.className = 'd-flex justify-content-between align-items-center w-100 flex-container';
+
+    const span = document.createElement('span');
+    span.className = 'product-text';
+    span.textContent = productText;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.className = 'btn btn-danger btn-sm delete-btn px-1 py-0';
+    deleteBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        ol.removeChild(li);
+        addedProductsTexts.delete(productText);
+    });
+
+    flexContainer.appendChild(span);
+    flexContainer.appendChild(deleteBtn);
+    li.appendChild(flexContainer);
     ol.appendChild(li);
     addedProductsTexts.add(productText);
 };
