0c3969fca1a5defaf9e936d47d5b11bf161992cf
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-08 20:16:03 -0400

    Adjust ExpressError

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const ExpressError = require('../utils/ExpressError');
 
 router.get('/:number', catchAsync(async (req, res, next) => {
     const number = req.params.number;
-    if(!number) throw new ExpressError('Invalid product number', 400);
+    if (!number) throw new ExpressError('Invalid product number', 400);
     const q = 'SELECT * FROM products WHERE number = ?';
     const result = await db.query(q, [number]);
     const product = result[0][0];
