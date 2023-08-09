2b88aeee9c90bf81328372b64d955779c3ad7ca2
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-09 18:25:53 -0400

    Add analysis directory

--- database/analysis/queries.sql
+++
@@ -1,9 +0,0 @@
-USE hbc;
-
--- Find the last transaction for each product
-SELECT
-    product_num,
-    MAX(trans_num) AS last_trans_num
-FROM transaction_products
-GROUP BY product_num
-ORDER BY last_trans_num DESC;
