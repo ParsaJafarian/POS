4617764f09c69898b80ff937a3bee4fed9cb0b23
Parsilius <108084316+Parsilius@users.noreply.github.com>
2023-08-13 21:48:55 -0400

    import completeTransaction

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const db = require('../utils/db');
+const ExpressError = require('../utils/ExpressError');
+const completeTransaction = require('../utils/completeTransaction');
 
 router.get('/new', (req, res) => {
     res.render('newTransaction')
@@ -10,9 +12,14 @@ router.get('/new', (req, res) => {
 router.get('/:num', catchAsync(async (req, res) => {
     const q = 'SELECT * FROM transactions WHERE num = ?';
     const result = await db.query(q, [req.params.num]);
-    const transaction = result[0][0];   
-    if(!transaction) throw new ExpressError('Transaction not found', 404);
+    const transaction = result[0][0];
+    if (!transaction) throw new ExpressError('Transaction not found', 404);
     res.send(transaction);
 }));
+
+router.post('/', catchAsync(async (req, res) => {
+    completeTransaction(req.user.num, req.body.product_nums);
+}));
+
 
 module.exports = router;
