c8b145a88bf07b1e971cd3ad5b6644fa52451555
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-08-14 13:48:59 -0400

    Fix logout

--- routes/auth.js
+++ routes/auth.js
@@ -17,9 +17,12 @@ router.post('/login', passport.authenticate('local', {
     successFlash: 'Welcome!'
 }));
 
-router.get('/logout', isLoggedIn, (req, res) => {
-    req.logout();
-    res.redirect('/auth/login');
+router.get('/logout', isLoggedIn, (req, res, next) => {
+    req.logout(err => {
+        if (err) return next(err);
+        req.flash('success', 'Successfully logged out');
+        res.redirect('/auth/login');
+    })
 });
 
 module.exports = router;
