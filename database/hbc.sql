d18e7d01b9a87af00d183ae1db25ea12693b2022
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-08-14 21:22:47 -0400

    Add tp_view.sql

--- database/hbc.sql
+++ database/hbc.sql
@@ -1,11 +1,17 @@
 DROP DATABASE IF EXISTS hbc;
+
 CREATE DATABASE IF NOT EXISTS hbc;
+
 USE hbc;
 
 SOURCE C:/Users/Admin/Desktop/HBC/database/tables/customers.sql;
+
 SOURCE C:/Users/Admin/Desktop/HBC/database/tables/employees.sql;
+
 SOURCE C:/Users/Admin/Desktop/HBC/database/tables/transactions.sql;
+
 SOURCE C:/Users/Admin/Desktop/HBC/database/tables/products.sql;
-SOURCE C:/Users/Admin/Desktop/HBC/database/tables/transaction_products.sql;
 
+SOURCE C:/Users/Admin/Desktop/HBC/database/tables/tp.sql;
 
+SOURCE C:/Users/Admin/Desktop/HBC/database/views/tp_view.sql;
