de87a46c89d102cd7cfcf27b0f9cdd6e4989454a
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-08-16 12:24:38 -0400

    Correct tp_view with the help of changes in tables/tp.sql

--- database/views/tp_view.sql
+++ database/views/tp_view.sql
@@ -3,14 +3,17 @@ DROP VIEW IF EXISTS tp_view;
 CREATE VIEW tp_view AS
 SELECT
     tp.trans_num,
-    employee_num,
-    DATE_FORMAT(date, '%d-%m-%Y') AS date,
+    tp.last_trans_num,
+    transactions.employee_num,
+    last_transactions.employee_num AS last_employee_num,
+    DATE_FORMAT(transactions.date, '%d-%m-%Y') AS date,
     COALESCE(amount_bought, 0) AS amount_bought,
     COALESCE(amount_returned, 0) AS amount_returned,
     COALESCE(amount_bought, 0) - COALESCE(amount_returned, 0) AS total
 FROM tp
-    JOIN transactions ON transactions.num = tp.trans_num
-    JOIN products ON products.num = tp.product_num
+    LEFT JOIN transactions ON transactions.num = tp.trans_num
+    LEFT JOIN products ON products.num = tp.product_num
+    LEFT JOIN transactions AS last_transactions ON last_transactions.num = tp.last_trans_num
     LEFT JOIN (
         SELECT
             trans_num,
@@ -18,7 +21,7 @@ FROM tp
         FROM tp
             JOIN transactions ON transactions.num = tp.trans_num
             JOIN products ON products.num = tp.product_num
-        WHERE is_available = 0
+        WHERE tp.is_return = 1
         GROUP BY
             trans_num
     ) AS returned ON returned.trans_num = tp.trans_num
@@ -29,9 +32,11 @@ FROM tp
         FROM tp
             JOIN transactions ON transactions.num = tp.trans_num
             JOIN products ON products.num = tp.product_num
-        WHERE is_available = 1
+        WHERE tp.is_return = 0
         GROUP BY
             trans_num
     ) AS bought ON bought.trans_num = tp.trans_num
-GROUP BY tp.trans_num
+GROUP BY
+    tp.trans_num,
+    tp.last_trans_num
 ORDER BY tp.trans_num DESC;
