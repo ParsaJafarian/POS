3fa9eca45652a0e887302c7f60dc7d3bf755b64c
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-08-16 12:23:57 -0400

    Add is_return and last_trans_num columns

--- database/tables/tp.sql
+++ database/tables/tp.sql
@@ -3,8 +3,11 @@ DROP TABLE IF EXISTS tp;
 CREATE TABLE
     IF NOT EXISTS tp (
         trans_num INT NOT NULL,
+        last_trans_num INT,
         product_num INT NOT NULL,
+        is_return BOOLEAN NOT NULL,
         FOREIGN KEY (trans_num) REFERENCES transactions(num),
         FOREIGN KEY (product_num) REFERENCES products(num),
+        FOREIGN KEY (last_trans_num) REFERENCES transactions(num),
         PRIMARY KEY (trans_num, product_num)
     );
